3dcce9ab9e4d38d0160bb3b4bc1aceccd2d17886
R. Kennedy <rkennedy@surgostream.com>
2024-07-10 14:06:37 -0400

    CHANGE - Function name to INCLUSION from surgo_include

--- Inclusion.php
+++ Inclusion.php
@@ -1,6 +1,6 @@
 <?php
 
-function surgo_include($settings) {
+function inclusion($settings) {
     if (!isset($settings['path']) || !isset($settings['files'])) {
         throw new InvalidArgumentException('Settings must include both "path" and "files" keys.');
     }
@@ -16,21 +16,3 @@ function surgo_include($settings) {
     return $fullPaths;
 }
 
-// Example usage
-$settings = array(
-    'path' => 'path/to/my/snippets',
-    'files' => array(
-        'api_requests',  // This is the API request snippet
-        'surgo_table_td',	
-        'surgo_table_css_1',
-        'pretty_array',
-        'surgo_inputs',
-        'time_ago',
-        'time_ago2',
-        'time_test',
-        'debug'
-    )
-);
-
-$fullPaths = surgo_include($settings);
-print_r($fullPaths);
